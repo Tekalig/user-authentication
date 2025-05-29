fa626f6f9e4db4e1ae1487b8c225a8cf555e0f84
Tekalig <tekahazi06@gmail.com>
2025-05-29 15:53:56 +0300

    Add middleware to verify JWT tokens for authentication

---
+++ server/src/middleware/verifyAuth.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res
+      .status(403)
+      .json({ success: false, message: "Unauthorizied - Token is required" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res
+        .status(401)
+        .json({ success: true, message: "Unauthorizied - Invalid token" });
+    }
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+module.exports = verifyToken;
